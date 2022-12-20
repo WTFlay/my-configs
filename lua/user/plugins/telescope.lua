da36dbc725f717e9c7132074ece663978ee62274
Flavien Schriever <flavien.schriever@beamy.io>
2022-12-20 10:41:50 +0100

    add telescope file ignore patterns

--- lua/user/plugins/telescope.lua
+++ lua/user/plugins/telescope.lua
@@ -1,9 +1,25 @@
-local status, builtin = pcall(require, 'telescope.builtin')
+local status, builtin = pcall(require, "telescope.builtin")
 if not status then
-  return
+	return
 end
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
+local telescope_status, telescope = pcall(require, "telescope")
+if not telescope_status then
+	return
+end
+
+telescope.setup({
+	defaults = {
+		file_ignore_patterns = {
+			"node_modules",
+			"build",
+			"dist",
+			"yarn.lock",
+		},
+	},
+})
